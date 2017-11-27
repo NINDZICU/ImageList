53927d9ab969fed1e6315fe845a37a39c18cddae
NINDZICU <hlopunov598@gmail.com>
2017-11-27 22:45:54 +0300

    add 2 buttons for open and exit

--- app/src/main/java/com/kfpu/imagelist/MainActivity.java
+++ app/src/main/java/com/kfpu/imagelist/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.kfpu.imagelist.adapters.ImageRecyclerAdapter;
 import com.kfpu.imagelist.models.Image;
@@ -18,27 +21,51 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.rv_images)
     RecyclerView rv;
+    @BindView(R.id.btn_exit)
+    Button btnExit;
+    @BindView(R.id.btn_open)
+    Button btnOpen;
+    @BindView(R.id.ll_buttons)
+    LinearLayout llButtons;
     private ImageRecyclerAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideButtons();
+                rv.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                adapter = new ImageRecyclerAdapter(MainActivity.this);
+                ArrayList<Image> images = new ArrayList<>();
+                images.add(new Image("CAT", "http://developer.alexanderklimov.ru/android/images/android_cat.jpg"));
+                images.add(new Image("SKY", "https://cs8.pikabu.ru/post_img/big/2016/11/29/10/1480439597175676615.jpg"));
+                images.add(new Image("Bioshock", "https://vignette.wikia.nocookie.net/904acc8f-f117-4fb4-8919-f4f3d90ccb46/scale-to-width-down/1024"));
+                images.add(new Image("Supercar", "http://moneyinc.com/wp-content/uploads/2016/08/2017-Bugatti-Chiron.jpg"));
+                images.add(new Image("Gravity Falls", "http://kto-ozvuchil.ru/wp-content/uploads/01-19.jpg"));
+                adapter.setmImages(images);
+                rv.setAdapter(adapter);
+            }
+        });
 
-        rv.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        adapter = new ImageRecyclerAdapter(MainActivity.this);
-        ArrayList<Image> images = new ArrayList<>();
-        images.add(new Image("CAT", "http://developer.alexanderklimov.ru/android/images/android_cat.jpg"));
-        images.add(new Image("SKY", "https://cs8.pikabu.ru/post_img/big/2016/11/29/10/1480439597175676615.jpg"));
-        images.add(new Image("Bioshock", "https://vignette.wikia.nocookie.net/904acc8f-f117-4fb4-8919-f4f3d90ccb46/scale-to-width-down/1024"));
-        images.add(new Image("Supercar", "http://moneyinc.com/wp-content/uploads/2016/08/2017-Bugatti-Chiron.jpg"));
-        images.add(new Image("Gravity Falls", "http://kto-ozvuchil.ru/wp-content/uploads/01-19.jpg"));
-        adapter.setmImages(images);
-        rv.setAdapter(adapter);
+
     }
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    public void hideButtons(){
+        llButtons.setVisibility(View.GONE);
     }
 }
